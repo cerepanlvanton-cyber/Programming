2ae46544b49bb3afcbc65ab0d6a95bbea83f5d15
cerepanlvanton-cyber <cerepanlvanton@gmail.com>
2025-12-11 15:02:58 +0700

    last commit

--- lab9/sort.c
+++ lab9/sort.c
@@ -10,8 +10,7 @@ void swap(int* arr, int i, int j) {
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
       
-        // Last i elements are already in place, so the loop
-        // will only num n - i - 1 times
+        
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1])
                 swap(arr, j, j + 1);
